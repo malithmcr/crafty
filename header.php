1e917efbdb815997ca68f3fe9c88e3d6be9a800c
malithmcr <malith.priyashan.dev@gmail.com>
2018-12-15 10:30:26 +0100

    menu naame

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'crafty-blog' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'crafty-blog' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
